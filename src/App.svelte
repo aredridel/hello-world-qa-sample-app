5bf05f75bfb384f477924e5eb223b1b9e1b5302d
Aria Stewart <aredridel@dinhe.net>
2019-10-31 09:47:35 -0400

    Hello, World!

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	let name = 'world';
+	let color = "black";
+</script>
+
+<h1>Hello <span style="color: {color}">{name}</span>!</h1>
+
+<button on:click={() => { color = "red" }}>
+	 Go!
+</button>
+
+<button on:click={() => { color = "black" }} >
+	Stop
+</button>
+
+<form on:submit|preventDefault>
+	
+	<fieldset>
+		<legend>
+			Fields
+		</legend>
+		<label>Name <input bind:value={name} type="text"></label>
+	</fieldset>
+</form>
